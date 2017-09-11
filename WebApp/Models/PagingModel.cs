5dc2c6fa265bbdcedc7b54ae2dd8d78c42db1223
Rajat Sharma <rajatfit4it@gmail.com>
2017-09-11 10:29:22 +0530

    Paging

---
+++ WebApp/Models/PagingModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class PagingModel
+    {
+        public int TotalRecords { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int RecordsPerPage { get; set; }
+
+        public string ActionName { get; set; }
+
+        public string ControllerName { get; set; }
+
+
+        public int PageLength
+        {
+            get
+            {
+
+                double _pagelength = (double)TotalRecords / (double)RecordsPerPage;
+                return (int)Math.Ceiling(_pagelength);
+
+            }
+        }
+
+    }
+}
